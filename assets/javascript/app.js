0304f0a2b2d49f4d0d59e4696bf1c706c3a259b9
Mone Duncans-Francis <mone.eunique12@gmail.com>
2019-12-06 18:31:53 -0800

    updating quesitons for trivia

---
+++ assets/javascript/app.js
@@ -0,0 +1,26 @@
+var trivia = {
+    questions:{
+        q1:'What is the name of The Powerpuff Girls evil counterparts?',
+        q2:'In Hey Arnold!, what city did Arnold live in?',
+        q3:'In Captain Planet, the planteers each possessed an element power: Earth, Fire, Wind, Water, and what?',
+        q4:'Who is Dexters archenemy?',
+        q5:'What is the name of this show?',
+        q6:'What is Dougs last name?',
+        q7:'What was the teachers name in The Magic School Bus?',
+        q8:'What did Pinky and the Brain do every night?',
+        q9:'Which anime series always came out right after Sailor Moon on Toonami?',
+        q10:'What were the names of Tommy and Dils Parents in Rugrats?',
+    },
+    options: {
+        q1:['The Gangreen Gang', 'The Amoeba Boys','The RowdyRuff Boys'],
+        q2:['Nashville','Boston','Hillwood','Albuquerque'],
+        q3:['Heart','Air','Cloud','Thunder'],
+        q4:['Anti-Dexter','Jeezes','DeeDee','Mandark'],
+        q5:['Card Captors','Tokyo Mew Mew','Sailor Moon','The Winx Club'],
+        q6:[''],
+        q7:[],
+        q8:[],
+        q9:[],
+        q10:[],
+    }
+}
