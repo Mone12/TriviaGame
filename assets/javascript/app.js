0f4beb75d1490b4300cd73d9e5faa0d0f258efe0
Mone Duncans-Francis <mone.eunique12@gmail.com>
2019-12-09 19:07:37 -0800

    updating javascript

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,7 +8,7 @@ var trivia = {
         q6:'What is Dougs last name?',
         q7:'What was the teachers name in The Magic School Bus?',
         q8:'What did Pinky and the Brain do every night?',
-        q9:'Which anime series always came out right after Sailor Moon on Toonami?',
+        q9:'Which anime series always came on right after Sailor Moon on Toonami?',
         q10:'What were the names of Tommy and Dils Parents in Rugrats?',
     },
     options: {
@@ -17,10 +17,30 @@ var trivia = {
         q3:['Heart','Air','Cloud','Thunder'],
         q4:['Anti-Dexter','Jeezes','DeeDee','Mandark'],
         q5:['Card Captors','Tokyo Mew Mew','Sailor Moon','The Winx Club'],
-        q6:[''],
-        q7:[],
-        q8:[],
-        q9:[],
-        q10:[],
-    }
+        q6:['Funnie','Skeeter','Johnson'],
+        q7:['Mrs Drizzle','Mrs Frizzle','School Bus Driver','Professor Molly'],
+        q8:['Try to escape from the lab','Conquer the world','Build a mouse trap','Cheese it'],
+        q9:['Dragonball Z','Yu-gi-oh!','Naruto','Pokemon'],
+        q10:['Diane and Steve Pickle','Didi and Stu Pickles', 'Debbie and Stan Olives','Deeds and Steward Okra'],
+    },
+    answers: {
+        q1:'The RowdyRuff Boys',
+        q2:'Hillwood',
+        q3:'Heart',
+        q4:'Mandark'
+        q5:'Sailor Moon',
+        q6:'Funnie',
+        q7:'Mrs Frizzle',
+        q8:'Try to escape from the lab'
+        q9:'Dragonball Z'
+        q10:'Didi and Stu Pickles',
+    },
+}
+
+var twentySeconds = 20;
+
+
+
+function countDown() {
+    if()
 }
