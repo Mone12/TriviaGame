a764d97e9509215a178d4c41813b13716979a901
Mone Duncans-Francis <mone.eunique12@gmail.com>
2019-12-12 12:32:03 -0800

    updating timer on javascript

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,46 +1,122 @@
-var trivia = {
-    questions:{
-        q1:'What is the name of The Powerpuff Girls evil counterparts?',
-        q2:'In Hey Arnold!, what city did Arnold live in?',
-        q3:'In Captain Planet, the planteers each possessed an element power: Earth, Fire, Wind, Water, and what?',
-        q4:'Who is Dexters archenemy?',
-        q5:'What is the name of this show?',
-        q6:'What is Dougs last name?',
-        q7:'What was the teachers name in The Magic School Bus?',
-        q8:'What did Pinky and the Brain do every night?',
-        q9:'Which anime series always came on right after Sailor Moon on Toonami?',
-        q10:'What were the names of Tommy and Dils Parents in Rugrats?',
+$(document).ready(function(){
+var trivia = [
+    {
+        question:'What is the name of The Powerpuff Girls evil counterparts?',
+        options:['The Gangreen Gang', 'The Amoeba Boys','The RowdyRuff Boys'],
+        answer:2,
     },
-    options: {
-        q1:['The Gangreen Gang', 'The Amoeba Boys','The RowdyRuff Boys'],
-        q2:['Nashville','Boston','Hillwood','Albuquerque'],
-        q3:['Heart','Air','Cloud','Thunder'],
-        q4:['Anti-Dexter','Jeezes','DeeDee','Mandark'],
-        q5:['Card Captors','Tokyo Mew Mew','Sailor Moon','The Winx Club'],
-        q6:['Funnie','Skeeter','Johnson'],
-        q7:['Mrs Drizzle','Mrs Frizzle','School Bus Driver','Professor Molly'],
-        q8:['Try to escape from the lab','Conquer the world','Build a mouse trap','Cheese it'],
-        q9:['Dragonball Z','Yu-gi-oh!','Naruto','Pokemon'],
-        q10:['Diane and Steve Pickle','Didi and Stu Pickles', 'Debbie and Stan Olives','Deeds and Steward Okra'],
-    },
-    answers: {
-        q1:'The RowdyRuff Boys',
-        q2:'Hillwood',
-        q3:'Heart',
-        q4:'Mandark'
-        q5:'Sailor Moon',
-        q6:'Funnie',
-        q7:'Mrs Frizzle',
-        q8:'Try to escape from the lab'
-        q9:'Dragonball Z'
-        q10:'Didi and Stu Pickles',
-    },
-}
 
+    {
+        question:'In Hey Arnold!, what city did Arnold live in?',
+        options:['Nashville','Boston','Hillwood','Albuquerque'],
+        answer:2,
+    },
+
+    {
+        question:'In Captain Planet, the planteers each possessed an element power: Earth, Fire, Wind, Water, and what?',
+        options:['Heart','Air','Cloud','Thunder'],
+        answer: 0,
+    },
+
+    {
+        question:'Who is Dexters archenemy?',
+        options:['Anti-Dexter','Jeezes','DeeDee','Mandark'],
+        answer:3,
+    },
+
+    {
+        question:'What is the name of this show?',
+        options:['Card Captors','Tokyo Mew Mew','Sailor Moon','The Winx Club'],
+        answer:2,
+    },
+    
+    {
+        question:'What is Dougs last name?',
+        options:['Funnie','Skeeter','Johnson'],
+        answer: 0,
+    },
+    
+    {
+        question:'What was the teachers name in The Magic School Bus?',
+        options:['Mrs Drizzle','Mrs Frizzle','School Bus Driver','Professor Molly'],
+        answer:1,
+    },
+    
+    {
+        question:'What did Pinky and the Brain do every night?',
+        options:['Try to escape from the lab','Conquer the world','Build a mouse trap','Cheese it'],
+        answer:1,
+    },
+
+    {
+        question:'Which anime series always came on right after Sailor Moon on Toonami?',
+        options:['Dragonball Z','Yu-gi-oh!','Naruto','Pokemon'],
+        answer:0,
+    },
+
+    {
+        question:'What were the names of Tommy and Dils Parents in Rugrats?',
+        options:['Diane and Steve Pickle','Didi and Stu Pickles', 'Debbie and Stan Olives','Deeds and Steward Okra'],
+        answer:1,
+    }];
+    
+// counter variables
 var twentySeconds = 20;
+var correct = 0;
+var incorrect = 0;
+var unanswered = 0;
 
+// holder variables
+var definePush = [];
+var userGuess = "";
+var timerRuns = false;
+var intervalid;
 
+// game starts when uses click "#start-button"
 
-function countDown() {
-    if()
+$("#start-button").on("click",function(){
+    $("#start-button").hide();
+    displayQuestion();
+    runTimer();
+    for (var i = 0; i < trivia.length; i++) {
+        definePush.push(trivia[i]);
+        
+    };
+
+    function displayQuestion(){
+
+    };
+    function runTimer(){
+    intervalid = setInterval(decrement,1000);
+    }
+})
+
+    function decrement(){
+    twentySeconds--;
+
+    $("#time-left").html("<h3>Time Remaining:" + twentySeconds + "</h3>");
+
+   
+        if (twentySeconds === 0) {
+        unanswered++;
+        stop();
+        console.log(unanswered);
+
+      }
 }
+
+    function stop(){
+        clearInterval(intervalid);
+}
+
+
+
+
+// When user clicks start button, 20 second starts
+// hide start page/show questions page
+// show question 1
+// create function for user input
+// if user guesses the right answer than hid question and show answer for five seconds
+// if timers runs out show answer and display next question plus restart timer
+// create function for results after last question
+});
