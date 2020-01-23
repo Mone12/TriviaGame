4fc3ee893d418321969f9033e805307fdce08a70
Mone Duncans-Francis <mone.eunique12@gmail.com>
2020-01-22 21:08:10 -0800

    adding gifs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,26 +4,29 @@ $(document).ready(function() {
     {
       question: "What is the name of The Powerpuff Girls evil counterparts?",
       options: ["The Gangreen Gang", "The Amoeba Boys", "The RowdyRuff Boys"],
-      answer: 2
+      answer: 2,
+      photo: "assets/images/rowdyruff.gif"
     },
 
     {
       question: "In Hey Arnold!, what city did Arnold live in?",
       options: ["Nashville", "Boston", "Hillwood", "Albuquerque"],
-      answer: 2
+      answer: 2,
+      photo: "assets/images/heyarnold.gif"
     },
 
     {
-      question:
-        "In Captain Planet, the planteers each possessed an element power: Earth, Fire, Wind, Water, and what?",
+      question: "In Captain Planet, the planteers each possessed an element power: Earth, Fire, Wind, Water, and what?",
       options: ["Heart", "Air", "Cloud", "Thunder"],
-      answer: 0
+      answer: 0,
+      photo: "assets/images/heart.gif"
     },
 
     {
       question: "Who is Dexters archenemy?",
       options: ["Anti-Dexter", "Jeezes", "DeeDee", "Mandark"],
-      answer: 3
+      answer: 3,
+      photo: "assets/images/mandark.gif"
     },
 
     {
@@ -34,13 +37,15 @@ $(document).ready(function() {
         "Sailor Moon",
         "The Winx Club"
       ],
-      answer: 2
+      answer: 2,
+      photo: "assets/images/sailormoon.gif"
     },
 
     {
       question: "What is Dougs last name?",
       options: ["Funnie", "Skeeter", "Johnson"],
-      answer: 0
+      answer: 0,
+      photo: "assets/images/doug.gif"
     },
 
     {
@@ -51,7 +56,8 @@ $(document).ready(function() {
         "School Bus Driver",
         "Professor Molly"
       ],
-      answer: 1
+      answer: 1,
+      photo: "assets/images/frizzle.gif"
     },
 
     {
@@ -62,14 +68,16 @@ $(document).ready(function() {
         "Build a mouse trap",
         "Cheese it"
       ],
-      answer: 1
+      answer: 1,
+      photo: "assets/images/pinkybrain.gif"
     },
 
     {
       question:
         "Which anime series always came on right after Sailor Moon on Toonami?",
       options: ["Dragonball Z", "Yu-gi-oh!", "Naruto", "Pokemon"],
-      answer: 0
+      answer: 0,
+      photo: "assets/images/dragonballz.gif"
     },
 
     {
@@ -80,12 +88,13 @@ $(document).ready(function() {
         "Debbie and Stan Olives",
         "Deeds and Steward Okra"
       ],
-      answer: 1
+      answer: 1,
+      photo: "assets/images/stupickle.gif"
     }
   ];
 
   // counter variables
-  var twentySeconds = 20;
+  var tenSeconds;
   var correct = 0;
   var incorrect = 0;
   var unanswered = 0;
@@ -93,15 +102,13 @@ $(document).ready(function() {
 
   // holder variables
 
-  var definePush = [];
-  var userGuess = "";
-  var timerRuns = false;
+ 
   var intervalid =0;
-  var Qtext;
+  
 
   // game starts when uses click "#start-button"
   
-
+  
   $("#start-button").on("click", function() {
     $("#start-button").hide();
     displayQuestion();
@@ -109,22 +116,25 @@ $(document).ready(function() {
     
   });
 
+
   function secondsTimer() {
-    twentySeconds = 20;
+    tenSeconds = 11;
     intervalid = setInterval(decrement, 1000);
   }
 
   // Displays seconds on HTML and counts 'twentySeconds' down
   function decrement() {
-    twentySeconds--;
+    tenSeconds--;
 
-    $("#time-left").html("<h3>Time Remaining:" + twentySeconds + "</h3>");
+    $("#time-left").html("<h3>Time Remaining:" + tenSeconds + "</h3>");
 
     // timer restarts to 20 seconds after hitting zero and unanswered var goes up 1
-    if (twentySeconds < 1) {
+    if (tenSeconds < 1) {
       unanswered++;
       messageUser("Time's up!!!");
       $("#answers-page").html("<h3>Correct answer is" + " " +trivia[index].options[trivia[index].answer] + "</h3>");
+      $("#image-page").html("<img src=" + trivia[index].photo + ">");
+      
       
     }
   }
@@ -168,11 +178,12 @@ $(document).ready(function() {
   function messageUser(msg) {
     stop();
     $("#message").html(`<h4>${msg}</h4>`);
-    setTimeout(next, 1000);
+    setTimeout(next, 3000);
   }
 
   function next() {
     $("#message").empty();
+    $("#image-page").empty();
     index++;
     if (index < trivia.length) {
       displayQuestion();
@@ -198,24 +209,23 @@ $(document).ready(function() {
     $("#results-page").append("<h3>Unanswered:"+ unanswered + "</h3>");
     
     stop();
-  }
-
-
-
-
-   function restart() {
-    $("#results-page").hide();
-    correct = 0;
-    incorrect = 0;
-    unanswered = 0;
-
-    $("start-button").show()
     
-      
-    
+   
   }
+  
+  
+  // $("#restart-button").on("click", function() {
+  //     $("#results-page").hide();
+
+  //     // correct = 0;
+  //     // incorrect = 0;
+  //     // unanswered = 0;
+  //     displayQuestion();
+  //     secondsTimer();
+    
+
+     
+  //  })
  
 
-  
-  
-});
+  });
